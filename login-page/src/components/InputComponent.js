c1f9beb6e6c82b5cd3ab6a83f37d069c0f3aa4b5
Swami Training <swamitraining@MacBook-Pro.local>
2019-01-23 11:56:53 +0530

    Added props

---
+++ login-page/src/components/InputComponent.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default (props) => {
+    const icon = props.icon;
+    return (
+        <div style={{
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            width: '100%',
+            padding: '12px',
+            backgroundColor: '#f1f1f1',
+            border: '1px solid #d3d3d3',
+            borderRadius: '5px',
+            marginTop: '15px'
+        }}>
+            <img src={icon} />
+            <input type="email" placeholder="test@email.com" style={{
+                borderWidth: 0,
+                borderStyle: 'none',
+                borderColor: 'none',
+                borderImage: 'none',
+                backgroundColor: '#f1f1f1',
+                fontSize: '18px',
+                marginLeft: '15px'
+            }} />
+        </div>
+    );
+};
--- login-page/src/components/LoginComponent.js
+++ login-page/src/components/LoginComponent.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import InputComponent from './InputComponent';
 
 class LoginComponent extends React.Component {
 
@@ -6,6 +7,39 @@ class LoginComponent extends React.Component {
         return (
             <div style={styles.rootContainer}>
                 <div style={styles.loginCardContainer}>
+                    <p style={{
+                        fontSize: '18px',
+                        fontWeight: 'bold'
+                    }}>Welcome back</p>
+                    <p style={styles.lightTextStyle}>Sign in to your account</p>
+                    <InputComponent icon="https://img.icons8.com/color/30/d3d3d3/secured-letter.png" />
+                    <InputComponent icon="https://img.icons8.com/ios-glyphs/30/d3d3d3/lock.png" />
+                    <div style={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        height: '50px',
+                        marginTop: '15px'
+                    }}>
+                        <div style={{ flex: 1 }}>
+                            <input type="checkbox" name="rememberMe"
+                                style={{ marginRight: '8px' }} />
+                            <label style={styles.lightTextStyle}>Remeber me</label>
+                        </div>
+                        <p
+                            style={{
+                                display: 'inline',
+                                color: 'green'
+                            }}
+
+                            onClick={() => {
+                                console.log("Clicked!!!");
+                            }}
+                        >
+                            Forgot your password?
+                        </p>
+                    </div>
+                    <div></div>
                 </div>
             </div>
         );
@@ -29,7 +63,13 @@ const styles = {
         bottom: 0,
         left: 0,
         right: 0,
-        margin: 'auto'
+        margin: 'auto',
+        padding: '50px'
+    },
+
+    lightTextStyle: {
+        color: '#A9A9A9',
+        fontSize: '16px'
     }
 }
 
